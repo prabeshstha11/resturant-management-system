0947eb6c0088fbf3c1422ae484ed5a50d4a81727
Prabesh Shrestha <prabeshshrestha90@gmail.com>
2026-05-25 13:07:11 +0545

    change verbose_name_plural

--- main/rms/models.py
+++ main/rms/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
     
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+    
     def __str__(self):
         return self.name
     
