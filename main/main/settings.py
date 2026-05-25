846690729224c8e4cd92f5e31213cc365a820851
Prabesh Shrestha <prabeshshrestha90@gmail.com>
2026-05-25 21:32:58 +0545

    adding drf and serializers

--- main/main/settings.py
+++ main/main/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rms"
+    "rms",
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
